19d98b1ec6fe6a469c92677827075c84d3500a3b
frocco <fabio.rocco@green-conseil.com>
2016-11-02 17:05:15 +0100

    ajout fichier index.php

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="Description" charset="Description de mon cv">
+    <title>Mon CV</title>
+</head>
+<body>
+    <header>
+        <marquee>
+            <h1>
+                CV - Fabio ROCCO
+            </h1>
+        </marquee>
+        <center>
+            <a href="#footer">
+                <img src="img/image.png" alt="Description de mon image">
+            </a><br>
+            <a href="" download="cv.pdf">Télécharger mon CV</a>
+        </center>
+    </header>
+    <section>
+        <hr>
+        <h2>Expériences PRO</h2>
+        <table border>
+            <thead>
+                <tr>
+                    <th colspan="2">Année / Entreprise</th>
+                    <th>Mission</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>2016</td>
+                    <td>Sixt</td>
+                    <td>Dév WEB</td>
+                </tr>
+                <tr>
+                    <td>2016</td>
+                    <td>Sixt</td>
+                    <td>Dév WEB</td>
+                </tr>
+                <tr>
+                    <td>2016</td>
+                    <td>Sixt</td>
+                    <td>Dév WEB</td>
+                </tr>
+            </tbody>
+        </table>
+        <hr>
+        <h2>Formations</h2>
+        <ul>
+            <li>Master IW
+            <li>Licence IW
+            <li>BTS SIO
+            <li>BAC S
+        </ul>
+        <hr>
+        <h2>Compétences</h2>
+        HTML5 & CSS<progress value="80" max="100"></progress><br>
+        PHP<progress value="80" max="100"></progress><br>
+        JS<progress value="80" max="100"></progress>
+        <hr>
+        <h2>Contact</h2>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10501.896943014895!2d2.3897343!3d48.8491666!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5891d8d62e8535c7!2sEcole+Sup%C3%A9rieure+de+G%C3%A9nie+Informatique+(ESGI)!5e0!3m2!1sfr!2sfr!4v1478083837422" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <form action="form.php" method="post">
+            <input type="email" name="email" placeholder="Votre Email"><br>
+            <input type="text" name="objet" placeholder="Votre objet"><br>
+            <textarea name="name"></textarea><br>
+            <input type="submit" value="Envoyer">
+        </form>
+    </section>
+    <footer id="footer">
+        <center>&copy; Fabio ROCCO 2016</center>
+    </footer>
+</body>
+</html>
